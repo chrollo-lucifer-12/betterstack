dd6d2186b9bffd3231caa0522e368219bdfadaed
sahil-singh1107 <sahilpython@gmail.com>
2026-03-21 18:58:03 +0530

    create website handler

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,12 +1,6 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import {
-  createSelectSchema,
-  createInsertSchema,
-  createUpdateSchema,
-} from "drizzle-zod";
-import { regions, statusEnum, websites, websiteTicks } from "./db/schema";
 import * as schema from "./db/schema";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -15,16 +9,3 @@ const pool = new Pool({
 export * from "drizzle-orm";
 
 export const db = drizzle(pool, { schema });
-
-export const websiteSelectSchema = createSelectSchema(websites);
-export const regionSelectSchema = createSelectSchema(regions);
-export const websiteTickSelectSchema = createSelectSchema(websiteTicks);
-export const statusSelectSchema = createSelectSchema(statusEnum);
-
-export const websiteInsertSchema = createInsertSchema(websites);
-export const regionInsertSchema = createInsertSchema(regions);
-export const websiteTickInsertSchema = createInsertSchema(websiteTicks);
-
-export const websiteUpdateSchema = createUpdateSchema(websites);
-export const regionUpdateSchema = createUpdateSchema(regions);
-export const websiteTickUpdateSchema = createUpdateSchema(websiteTicks);
