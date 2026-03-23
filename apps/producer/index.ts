df027e8184ca197b389782ce6309eff7222f280b
sahil-singh1107 <sahilpython@gmail.com>
2026-03-23 15:10:17 +0530

    improve worker

--- apps/producer/index.ts
+++ apps/producer/index.ts
@@ -1,29 +1,44 @@
 import { RedisClient } from "bun";
-import { db } from "@repo/db";
+import { db, gt } from "@repo/db";
 import { websites } from "@repo/db/schema";
 
+let lastId: string | undefined = undefined;
+const client = new RedisClient(process.env.REDIS_URL!);
+
 async function startRedis() {
-  const allWebsites = await db.select().from(websites);
+  const query = db
+    .select({ id: websites.id, url: websites.url })
+    .from(websites);
 
-  const client = new RedisClient(process.env.REDIS_URL!);
+  const allWebsites = lastId
+    ? await query.where(gt(websites.id, lastId))
+    : await query;
 
-  const res = await client.send("xadd", [
-    "betterstack:websites",
-    "*",
-    "websites",
-    JSON.stringify(
-      allWebsites.map((w) => {
-        return {
-          id: w.id,
-          url: w.url,
-        };
-      }),
-    ),
-  ]);
+  if (!allWebsites || allWebsites.length === 0) {
+    console.log("No new websites");
+    return;
+  }
 
-  console.log(res);
+  const pipeline: Promise<any>[] = [];
 
-  client.close();
+  for (const w of allWebsites) {
+    pipeline.push(
+      client.send("xadd", [
+        "betterstack:websites",
+        "*",
+        "id",
+        w.id,
+        "url",
+        w.url,
+      ]),
+    );
+  }
+
+  await Promise.all(pipeline);
+
+  lastId = allWebsites[allWebsites.length - 1]?.id;
+
+  console.log(`Pushed ${allWebsites.length} websites`);
 }
 
 startRedis();
--- apps/worker/index.ts
+++ apps/worker/index.ts
@@ -3,27 +3,54 @@ import { db, eq } from "@repo/db";
 import { regions, websiteTicks } from "@repo/db/schema";
 
 type RedisStream = Record<string, [string, string[]][]>;
+const region = process.env.REGION!;
+
+let [regionRow] = await db
+  .select({ id: regions.id })
+  .from(regions)
+  .where(eq(regions.name, region));
+
+if (!regionRow) {
+  const [newRegion] = await db
+    .insert(regions)
+    .values({ name: region })
+    .returning({ id: regions.id });
+
+  if (!newRegion) throw new Error(`Failed to create region: ${region}`);
+  regionRow = newRegion;
+}
+
+const client = new RedisClient(process.env.REDIS_URL!);
+
+const getStatus = async (url: string) => {
+  const start = performance.now();
+  const res = await fetch(url);
+  const responseTime = performance.now() - start;
+
+  const status = res.ok ? "UP" : "DOWN";
+  return { status, responseTime } as const;
+};
+
+const getStatusAndInsert = async (
+  url: string,
+  timestamp: string,
+  websiteId: string,
+) => {
+  const { responseTime, status } = await getStatus(url);
+
+  console.log(url, status);
+
+  await db.insert(websiteTicks).values({
+    regionId: regionRow.id,
+    websiteId,
+    created_at: new Date(Number(timestamp)),
+    status,
+    responseTimeMs: Math.round(responseTime),
+  });
+};
 
 async function main() {
-  const region = process.env.REGION!;
-
-  let [regionRow] = await db
-    .select({ id: regions.id })
-    .from(regions)
-    .where(eq(regions.name, region));
-
-  if (!regionRow) {
-    const [newRegion] = await db
-      .insert(regions)
-      .values({ name: region })
-      .returning({ id: regions.id });
-
-    if (!newRegion) throw new Error(`Failed to create region: ${region}`);
-    regionRow = newRegion;
-  }
-
-  const client = new RedisClient(process.env.REDIS_URL!);
-
+  console.log("Polling...");
   const res = (await client.send("xreadgroup", [
     "group",
     region,
@@ -34,49 +61,37 @@ async function main() {
     "betterstack:websites",
     ">",
   ])) as RedisStream | null;
-  if (!res) return;
+  console.log("RAW:", res);
+  if (!res) {
+    console.log("no streams found");
+    return;
+  }
 
   const stream = res["betterstack:websites"];
 
   if (!stream || stream.length === 0) {
     console.log("No messages");
-    client.close();
     return;
   }
 
-  for (const [id, fields] of stream) {
-    const obj = Object.fromEntries(
-      fields.reduce<[string, string][]>((acc, _, i) => {
-        if (i % 2 === 0) acc.push([fields[i]!, fields[i + 1]!]);
-        return acc;
-      }, []),
-    );
+  await Promise.allSettled(
+    stream.map(([id, fields]) => {
+      const obj = Object.fromEntries(
+        fields.reduce<[string, string][]>((acc, _, i) => {
+          if (i % 2 === 0) acc.push([fields[i]!, fields[i + 1]!]);
+          return acc;
+        }, []),
+      );
 
-    if (obj.websites) {
-      const websites: { id: string; url: string }[] = JSON.parse(obj.websites);
+      if (!obj.url || !obj.id) return Promise.resolve();
 
       const timestamp = id.split("-")[0];
-      for (const site of websites) {
-        const start = performance.now();
-        const res = await fetch(site.url);
-        const responseTime = performance.now() - start;
 
-        const status = res.ok ? "UP" : "DOWN";
-
-        console.log(site.url, status);
-
-        await db.insert(websiteTicks).values({
-          regionId: regionRow.id,
-          websiteId: site.id,
-          created_at: new Date(Number(timestamp!)),
-          status,
-          responseTimeMs: Math.round(responseTime),
-        });
-      }
-    }
-  }
-
-  client.close();
+      return getStatusAndInsert(obj.url, timestamp!, obj.id).then(() =>
+        client.send("xack", ["betterstack:websites", region, id]),
+      );
+    }),
+  );
 }
 
 main().catch(console.error);
